c1446d32658fbd9ee8df9fce98de9431ece99b36
youlong <youlong@orbbec.com>
2022-12-09 09:50:07 +0800

    feat:增加windows平台创建recipes的批处理脚本

--- build_x86.bat
+++ build_x86.bat
@@ -1,8 +1,47 @@
-cd recipes/ceres-solver
+pushd %~dp0
+
+pushd recipes/catch2
+conan create 2.x.x/conanfile.py catch2/2.13.9@ -pr:b=default -pr:h=default
+popd
+
+pushd recipes/ceres-solver
 conan create all/conanfile.py ceres-solver/1.13.0@ -pr:b=default -pr:h=default
+popd
 
-cd recipes/cli11
+pushd recipes/cli11
 conan create all/conanfile.py cli11/1.9.1@ -pr:b=default -pr:h=default
+popd
 
-cd recipes/opencv
+pushd recipes/dlib
+conan create all/conanfile.py dlib/19.19@ -pr:b=default -pr:h=default
+popd
+
+pushd recipes/eigen
+conan create all/conanfile.py eigen/3.4.0@ -pr:b=default -pr:h=default
+popd
+
+pushd recipes/ghc-filesystem
+conan create all/conanfile.py ghc-filesystem/1.5.12@ -pr:b=default -pr:h=default
+popd
+
+pushd recipes/nlohmann_json
+conan create all/conanfile.py nlohmann_json/3.9.1@ -pr:b=default -pr:h=default
+popd
+
+pushd recipes/opencv
 conan create 3.x/conanfile.py opencv/3.4.12@ -pr:b=default -pr:h=default
+popd
+
+pushd recipes/stb
+conan create all/conanfile.py stb/cci.20220909@ -pr:b=default -pr:h=default
+popd
+
+pushd recipes/toml11
+conan create all/conanfile.py toml11/3.7.1@ -pr:b=default -pr:h=default
+popd
+
+pushd recipes/vcglib
+conan create all/conanfile.py vcglib/2020.12@ -pr:b=default -pr:h=default
+popd
+
+popd
